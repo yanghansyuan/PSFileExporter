d19efbf262b1eea675d5d94bdee240cccedc7997
TRIBE\hansyuan.yang <yang.hansyuan@gmail.com>
2018-08-10 11:17:55 +0800

    add

--- File_Exporter.jsx
+++ File_Exporter.jsx
@@ -7,29 +7,67 @@ var originalWidth
 var originalHeight 
 
 var groupAmount
-var mySectionRoot
+var myRoot
 
+var desc
+var descCheck 
 
 
 function myInput(){
 
-    originalDoc = app.activeDocument
+    try
+    {
+        originalDoc = app.activeDocument
+    }
+    catch(err)
+    {
+        alert("Please open document first!")
+        return
+    }
     groupAmount = app.activeDocument.layerSets.length    
     originalWidth = app.activeDocument.width;
     originalHeight = app.activeDocument.height;
-    //
-    var myWindow = new Window("dialog","YHS's File_Exporter  v1.0"); 
+    originalDocMode = app.activeDocument.mode
+    
+    //window
+    var myWindow = new Window("dialog","YHS's File_Exporter  v1.2"); 
     myWindow.orientation = "row" 
+    //radioBut
+    var rdiGroup = myWindow.add("group"); 
+    rdiGroup.orientation = "row"
+    var rdi_RGB = rdiGroup.add("radiobutton",undefined,"RGB")
+    var rdi_CMYK = rdiGroup.add("radiobutton",undefined,"CMYK")
+    if(originalDocMode == DocumentMode.RGB)
+    {
+        rdi_RGB.value = true
+    }
+    else{
+        rdi_CMYK.value = true
+    }
 
+    //editText
     var myPathGroup = myWindow.add("group");
     myPathGroup.orientation = "column"
-    // mySectionRoot = myPathGroup.add("edittext",undefined,"/Users/yhs/Desktop/export/") 
-    mySectionRoot = myPathGroup.add("edittext",undefined,"D:/MyWorkFolder/Escape/PS_Action/Export/")
-    // mySectionRoot = myPathGroup.add("edittext",undefined,"/Users/mac/Desktop/詩特莉/2018_婚約禮盒/02完稿/") 
-    
-    mySectionRoot.characters =30;
-    mySectionRoot.active = true
+    //save path preference
 
+    //defined prefence
+    desc = new ActionDescriptor();
+    
+    try
+    {
+        descCheck = app.getCustomOptions("myPSSetting")
+        newPath = descCheck.getString(1)
+        myRoot = myPathGroup.add("edittext",undefined,newPath) //show path with saved preference     
+    }
+    catch(err)
+    {
+        myRoot = myPathGroup.add("edittext",undefined,"/Users/yhs/Desktop/export/") 
+    }
+
+    // myRoot = myPathGroup.add("edittext",undefined,"/Users/yhs/Desktop/export/") 
+    myRoot.characters =30;
+    myRoot.active = true
+    //button
     var mybutGroup = myWindow.add("group");
     mybutGroup.orientation = "column"
     var but_export = mybutGroup.add('button',undefined,"Export");
@@ -40,6 +78,7 @@ function myInput(){
     }
 
     myWindow.show();
+ 
 }
 
 function callProcess()
@@ -54,7 +93,14 @@ function mainProcess(_index){
     
 
     //-----duplicat and merge------
-    var newDoc = app.documents.add(originalWidth,originalHeight,300,"Untitled-1" +_index, NewDocumentMode.CMYK) //add new doc  \
+    if(originalDocMode == DocumentMode.RGB)
+    {
+        var newDoc = app.documents.add(originalWidth,originalHeight,300,"Untitled-1" +_index, NewDocumentMode.RGB) //add new doc  
+    }
+    else
+    {
+        var newDoc = app.documents.add(originalWidth,originalHeight,300,"Untitled-1" +_index, NewDocumentMode.CMYK) //add new doc  
+    }
 
     app.activeDocument = originalDoc
     var sourceGroup = app.activeDocument.layerSets[_index]//get group
@@ -73,7 +119,6 @@ function mainProcess(_index){
     }
     catch(err)
     {
-
     }
     
     try
@@ -89,9 +134,12 @@ function mainProcess(_index){
 
     //-----save-----   
     app.activeDocument = newDoc //swtich back to target doc 
-    fileRef = File( mySectionRoot.text +  _index + "_" + fileName);    
+    fileRef = File( myRoot.text + "\\" + _index + "_" + fileName);    
     var psdOpts = new PhotoshopSaveOptions;
     app.activeDocument.saveAs(fileRef,psdOpts,true);
+    //save path
+    desc.putString(1,myRoot.text)
+    app.putCustomOptions("myPSSetting",desc,true)
 
     //-----close document-----
     app.activeDocument = newDoc
@@ -104,4 +152,4 @@ function mainProcess(_index){
 //test
 // var artLayerRef = activeDocument.artLayers.add()
 // artLayerRef.kind = LayerKind.TEXT
-// var textItemRef = artLayerRef.textItem.contents = mySectionRoot.text;
+// var textItemRef = artLayerRef.textItem.contents = myRoot.text;
